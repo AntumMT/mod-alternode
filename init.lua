d0003bfa1e78dc9c0814eb72c7fa55d1b99885ee
Jordan Irwin <antumdeluge@gmail.com>
2021-05-02 02:11:22 -0700

    Many fixes & show item name when using infostick

--- init.lua
+++ init.lua
@@ -25,11 +25,17 @@ core.register_craftitem(alternode.name .. ":infostick", {
 		end
 
 		local pos = core.get_pointed_thing_position(pointed_thing, false)
+		local node = core.get_node_or_nil(pos)
+		if not node then
+			core.chat_send_player(pname, "That doesn't seem to be a proper node")
+			return
+		end
 		local meta = core.get_meta(pos)
 
 		local infostring = "pos: x=" .. tostring(pos.x)
 			.. ", y=" .. tostring(pos.y)
 			.. ", z=" .. tostring(pos.z)
+			.. "; name=" .. node.name
 
 		for _, key in ipairs({"infotext", "owner"}) do
 			local value = meta:get_string(key)
@@ -53,30 +59,22 @@ function alternode.set(pos, key, value)
 	return meta:get_string(key) == value
 end
 
-function alternode.set_int(pos, key, value)
-	local meta = core.get_meta(pos)
-	meta:set_int(key, value)
-	return meta:get_int(key) == value
-end
-
-function alternode.set_float(pos, key, value)
-	local meta = core.get_meta(pos)
-	meta:set_float(key, value)
-	return meta:get_float(key) == value
-end
-
 
 core.register_chatcommand("setmeta", {
-	params = "<x> <y> <z> string|int|float <key> <value>",
+	params = "<x> <y> <z> <key> <value>",
 	description = "Alter meta data of a node",
 	privs = {server=true,},
 	func = function(player, param)
 		local plist = string.split(param, " ")
 
+		if #plist < 3 then
+			core.chat_send_player(player, "You must supply proper coordinates")
+			return false
+		end
+
 		for _, p in ipairs({plist[1], plist[2], plist[3]}) do
 			if tonumber(p) == nil then
-				core.chat_send_player(player,
-					"Coordinate parameters must be numbers")
+				core.chat_send_player(player, "You must supply proper coordinates")
 				return false
 			end
 		end
@@ -87,28 +85,27 @@ core.register_chatcommand("setmeta", {
 			z = tonumber(plist[3]),
 		}
 
-		local vtype = plist[4]
-		local key = plist[5]
-		local value = plist[6]
-		local retval = false
-		if vtype == "int" then
-			retval = alternode.set_int(pos, key, tonumber(value))
-		elseif vtype == "float" then
-			retval = alternode.set_float(pos, key, tonumber(value))
-		elseif vtype == "string" then
-			local rem = {}
-			for idx, word in ipairs(plist) do
-				if idx > 5 then
-					table.insert(rem, word)
-				end
-			end
+		local key = plist[4]
+		if key then key = key:trim() end
 
-			retval = alternode.set(pos, key, table.concat(rem, " "))
-		else
-			core.chat_send_player(player,
-				"Unknown meta data type: " .. vtype)
+		if not key or key == "" then
+			core.chat_send_player(player, "You must supply a key parameter")
 			return false
 		end
+
+		local value = {}
+		for idx, word in ipairs(plist) do
+			if idx > 4 then
+				table.insert(value, word)
+			end
+		end
+
+		if #value == 0 then
+			core.chat_send_player(player, "You must supply a value parameter")
+			return false
+		end
+
+		local retval = alternode.set(pos, key, table.concat(value, " "):trim())
 
 		if not retval then
 			core.chat_send_player(player,
@@ -136,6 +133,19 @@ core.register_chatcommand("getmeta", {
 	privs = {server=true,},
 	func = function(player, param)
 		local plist = string.split(param, " ")
+
+		if #plist < 3 then
+			core.chat_send_player(player, "You must supply proper coordinates")
+			return false
+		end
+
+		for _, p in ipairs({plist[1], plist[2], plist[3]}) do
+			if tonumber(p) == nil then
+				core.chat_send_player(player, "You must supply proper coordinates")
+				return false
+			end
+		end
+
 		local pos = {
 			x = tonumber(plist[1]),
 			y = tonumber(plist[2]),
@@ -143,6 +153,13 @@ core.register_chatcommand("getmeta", {
 		}
 
 		local key = plist[4]
+		if key then key = key:trim() end
+
+		if not key or key == "" then
+			core.chat_send_player(player, "You must supply a key parameter")
+			return false
+		end
+
 		local value = alternode.get(pos, key)
 		if not value or value == "" then
 			core.chat_send_player(player,
